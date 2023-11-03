ae025c575b9af187461e737a8e05b0fb4c27c38e
YaelCano <78746517+YaelCano@users.noreply.github.com>
2023-11-03 12:18:22 -0500

    feat: :sparkles: Add new code Linq,Program

--- Program.cs
+++ Program.cs
@@ -1,10 +1,75 @@
-﻿using Ferre_Linq.Clases;
+﻿using Ferre_Linq.entities;
 
 internal class Program
 {
-    static List<Productos> productos = new List<Productos>();
     private static void Main(string[] args)
     {
-        
+        LinQ funciones = new();
+        bool bander = false;
+
+        while (bander == false)
+        {
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine("╔══════════════════════════════════╗");
+            Console.WriteLine("║      MENÚ DE LA FERRETERÍA       ║");
+            Console.WriteLine("╚══════════════════════════════════╝");
+            Console.ResetColor();
+            Console.WriteLine("╔══════════════════════════════════╗");
+            Console.WriteLine("| 1. List Inventory Products       |");
+            Console.WriteLine("| 2. List Products  To be Finished |");
+            Console.WriteLine("| 3. List products you should buy  |");
+            Console.WriteLine("| 4. List Recipts January/2023     |");
+            Console.WriteLine("| 5. List Products from a receipt  |");
+            Console.WriteLine("| 6. Calculate the total Inventory |");
+            Console.WriteLine("| 7. Exit                          |");
+            Console.WriteLine("╚══════════════════════════════════╝");
+            Console.WriteLine($"Elige un opcion: ");
+            var opcion = Console.ReadLine();
+            Console.ForegroundColor = ConsoleColor.White;
+            switch (opcion)
+            {
+                case "1":
+                    Console.Clear();
+                    funciones.ListProducts();
+                    Console.ReadKey();
+                    break;
+                case "2":
+                    Console.Clear();
+                    funciones.ListEndProducts();
+                    Console.ReadKey();
+                    break;
+                case "3":
+                    Console.Clear();
+                    funciones.ListProductsBuy();
+                    Console.ReadKey();
+                    break;
+                case "4":
+                    Console.Clear();
+                    funciones.ReceiptsJanuary();
+                    Console.ReadKey();
+                    break;
+                case "5":
+                    Console.Clear();
+                    funciones.GetProductsFromReceipts();
+                    Console.ReadKey();
+                    break;
+                case "6":
+                    Console.Clear();
+                    funciones.CalculateTotalPriceStock();
+                    Console.ReadKey();
+                    break;
+                case "7":
+                    Console.Clear();
+                    Console.WriteLine("Gracias.....");
+                    bander = true;
+                    break;
+                default:
+                    Console.WriteLine("No haz seleccionado ninguna de las opciones.");
+                    Console.WriteLine($"Adios y vuelve pronto :....");
+                    bander = true;
+                    break;
+            }
+        }
     }
 }
--- entities/LinQ.cs
+++ entities/LinQ.cs
@@ -15,7 +15,7 @@ namespace Ferre_Linq.entities
             new(){Id = 3, Name = "Bisagras", Cantity = 65, UnitPrice = 1000, MinStock = 40, MaxStock = 300},
             new(){Id = 4, Name = "Pinturas", Cantity = 35, UnitPrice = 2000, MinStock = 40, MaxStock = 300},
             new(){Id = 5, Name = "EstucoBlanco", Cantity = 120, UnitPrice = 400, MinStock = 40, MaxStock = 300}
-            
+
         };
         List<Receipt> Receipts = new(){
             new(){
@@ -40,5 +40,60 @@ namespace Ferre_Linq.entities
             new(){Id = 4, NumReceipt = 123, ProductId = 4, Cantity = 4},
             new(){Id = 5, NumReceipt = 234, ProductId = 1, Cantity = 3}
         };
+        public void ListProducts()
+        {
+            Products.ForEach(x => Console.WriteLine($"Name: {x.Name}, Id: {x.Id}, Cantity: {x.Cantity}, UnitPrice: {x.UnitPrice}"));
+        }
+        public void ListEndProducts()
+        {
+            var FnProduct = (from j in Products where j.Cantity < j.MinStock select j).ToList();
+            Console.WriteLine($"The products that are close to Be finished are:");
+            FnProduct.ForEach(J => Console.WriteLine($"{J.Name}: Cantity: {J.Cantity}, MinStock: {J.MinStock}"));
+        }
+
+        public void ListProductsBuy()
+        {
+            var ProductsTobuy = (from p in Products where p.Cantity < p.MinStock select p).ToList();
+            ProductsTobuy.ForEach(M => Console.WriteLine($"The {M.Name} are close to Be Finished (there are only {M.Cantity}), you should {M.MaxStock - M.Cantity} to rach the maximun stock"));
+        }
+        public void ReceiptsJanuary()
+        {
+            var ReceiptsJan = (from d in Receipts where d.Date.Month == 01 && d.Date.Year == 2023 select d).ToList();
+            foreach (var item in ReceiptsJan)
+            {
+                Console.WriteLine($"{(ReceiptsJan.IndexOf(item) + 1)}. NumReceipt: {item.NumReceipt}, TotalReceipt: {item.TotalReceipt}, Date: {item.Date.Day}/{item.Date.Month}/{item.Date.Year}");
+            }
+        }
+        public void GetProductsFromReceipts()
+        {
+            Console.WriteLine("Put the number of the receipt: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            var list = (from q in Details
+                        join w in Receipts
+                        on q.NumReceipt equals w.NumReceipt
+                        join e in Products on q.ProductId equals e.Id
+                        where w.NumReceipt == number
+                        select new
+                        {
+                            ProductId = q.ProductId,
+                            Cantity = q.Cantity,
+                            receipt = q.NumReceipt,
+                            Name = e.Name,
+                            UnitPrice = e.UnitPrice,
+                            Total = q.Cantity * e.UnitPrice
+                        }).ToList();
+            Console.WriteLine($"The products of the receipt number {number}");
+            list.ForEach(z => Console.WriteLine($"{new { z.ProductId, z.Cantity, z.Name, z.UnitPrice, z.Total }}"));
+        }
+        public void CalculateTotalPriceStock()
+        {
+            int total = 0;
+            foreach (var item in Products)
+            {
+                total += (item.Cantity * item.UnitPrice);
+                Console.WriteLine($"The Total Price of {item.Name} in the inventory is: {item.UnitPrice * item.Cantity}");
+            }
+            Console.WriteLine($"The Total Price of all the inventory is: {total}");
+        }
     }
 }
